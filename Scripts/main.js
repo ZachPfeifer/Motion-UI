c751a6162aeac3d5b0972150498f26b9511be118
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-28 14:37:18 -0700

    MVP // Integrating Responsive

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,8 @@
+//https://github.com/foundation/motion-ui/tree/master/docs
+
+
+// Hero Section
+
 let lastScrollPos = window.scrollY;
 let titleSection = document.querySelector(".title-section");
 let titleSectionBottom = titleSection.scrollHeight;
@@ -50,7 +55,9 @@ document.addEventListener("scroll", function (e) {
   lastScrollPos = currentScrollPos;
 });
 
-document.querySelectorAll('nav-link').forEach(link => {
+// Main header / Title Section
+
+document.querySelectorAll(".nav-link").forEach(link => {
   link.addEventListener("mouseover", () => {
     anime({
       targets: link,
@@ -60,10 +67,102 @@ document.querySelectorAll('nav-link').forEach(link => {
         { translateY: "0px" },
         { translateY: "2px" },
         { translateY: "-4px" },
-        { translateY: "0px" },
+        { translateY: "0px" }
       ],
       duration: 400,
       easing: "easeInOutExpo"
-    })
+    });
+  });
+});
+
+anime
+  .timeline()
+  .add({
+    targets: ".main-header",
+    opacity: [0, 1],
+    translateY: ["-5rem", 0],
+    easing: "spring(1, 80, 10, 0)",
+    duration: 300
   })
-})
+  .add(
+    {
+      targets: [".nav-link", ".brand"],
+      opacity: [0, 1],
+      translateY: ["-2rem", 0],
+      easing: "spring(1, 80, 10, 0)",
+      duration: 500
+    },
+    "-=800"
+  )
+  .add(
+    {
+      targets: ".title-header .line",
+      opacity: [0.5, 1],
+      scaleX: [0, 1],
+      easing: "easeInOutExpo",
+      duration: 700
+    },
+    "-=1400"
+  )
+  .add(
+    {
+      targets: ".title-header .line",
+      duration: 600,
+      easing: "easeOutExpo",
+      translateY: (el, i) => -0.625 + 0.625 * 2 * i + "em"
+    },
+    "-=700"
+  )
+  .add(
+    {
+      targets: ".title-header .letters-left",
+      opacity: [0, 1],
+      translateX: ["0.65em", "0.15em"],
+      easing: "easeOutExpo",
+      duration: 600
+    },
+    "-=300"
+  )
+  .add(
+    {
+      targets: ".title-header .letters-right",
+      opacity: [0, 1],
+      translateX: ["-0.65em", "-0.15em"],
+      easing: "easeOutExpo",
+      duration: 600
+    },
+    "-=600"
+  );
+
+// Articles Section
+
+let observer;
+const articles = Array.from(document.querySelectorAll(".article"));
+let options = {
+  threshold: 0.5
+};
+let current = -1;
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0.5) {
+      const newcurrent = articles.indexOf(entry.target);
+      if (newcurrent > current) {
+        anime({
+          targets: entry.target,
+          opacity: [0, 1],
+          translateY: ["20rem", 0],
+          easing: "spring(1, 80, 10, 0)",
+          duration: 800
+        });
+      }
+      current = newcurrent;
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+// start observing
+document.querySelectorAll(".article").forEach(article => {
+  observer.observe(article);
+});
